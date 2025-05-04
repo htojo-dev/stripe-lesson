974bac4432bc53b53cf683de1b7a633d37560ad3
htojo-dev <web.h.tojo1209@gmail.com>
2025-05-04 16:09:32 +0900

    OAuth認証接続

---
+++ components/Header.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import AuthServerButton from "./auth/AuthServerButton";
+
+const Header = () => {  
+  return (
+    <div className="flex py-4 px-6 border-b border-gray-200">
+      <Link href={`/`}>
+        <Button variant="outline">ホーム</Button>
+      </Link>
+      <Link href={`/pricing`} className="ml-4">
+        <Button variant="outline">価格</Button>
+      </Link>
+      <div className="ml-auto">
+        <AuthServerButton />
+      </div>
+      
+    </div>
+  );
+};
+
+export default Header;
